e93d54947e78b3e05d8bccd26c85413873c10b4a
Chino Ik <45470819+slightlybae@users.noreply.github.com>
2021-10-14 09:14:22 +0100

    Add files via upload

---
+++ contact_info.py
@@ -0,0 +1,10 @@
+
+contact_info = {
+  'name': 'Chino Ikwuegbu',
+  'city': 'Port Harcourt',
+  'favourite_color':'Blue',
+}
+
+print("My name is " + (contact_info['name'] ))
+print("I live in " + contact_info['city'])
+print("My favourite color is " + contact_info['favourite_color'])
